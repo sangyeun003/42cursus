f2181a48dddf8227a0778cd3b2b3fde0c9dd4431
Sangyeun Park <sangyepa@c5r6s5.42seoul.kr>
2023-03-20 23:14:26 +0900

    aaa

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: sangyepa <sangyepa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 18:34:12 by sangyepa          #+#    #+#             */
-/*   Updated: 2023/03/20 23:09:01 by sangyepa         ###   ########.fr       */
+/*   Updated: 2023/03/20 23:14:18 by sangyepa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char	*t_dst;
 	unsigned char	*t_src;
 
+	if (dst == 0 && src == 0)
+		return (0);
 	t_dst = (unsigned char *)dst;
 	t_src = (unsigned char *)src;
-	if (t_dst == 0 && t_src == 0)
-		return (0);
 	i = 0;
 	while (i < n)
 	{
