b826ae13124306014de340e8bbadb26ca1fb7a8b
sangyeun003 <sangyeun003@gmail.com>
2023-03-30 10:04:00 +0900

    mapi

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sangyepa <sangyepa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/29 23:22:49 by sangyepa          #+#    #+#             */
+/*   Updated: 2023/03/29 23:24:41 by sangyepa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*result;
+	int		len;
+
+	len = ft_strlen((char *)s);
+}
