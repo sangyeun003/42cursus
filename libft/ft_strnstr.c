2c2e63a8aacb8304898d5f252c5f8fb4d6d3cff0
sangyeun003 <sangyeun003@gmail.com>
2023-04-04 22:33:34 +0900

    111

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: sangyepa <sangyepa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 11:15:02 by sangyepa          #+#    #+#             */
-/*   Updated: 2023/04/04 21:42:32 by sangyepa         ###   ########.fr       */
+/*   Updated: 2023/04/04 22:05:58 by sangyepa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,10 @@ char	*strnstr(const char *haystack, const char *needle, size_t len)
 	int	j;
 	int	same;
 
-	
+	i = 0;
+	j = 0;
+	while (haystack[i] && i < len)
+	{
+		
+	}
 }
