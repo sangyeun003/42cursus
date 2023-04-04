ed411613a176071bd0675ecb4b786f7748e04725
sangyeun003 <sangyeun003@gmail.com>
2023-04-04 21:52:32 +0900

    i

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,13 +6,33 @@
 /*   By: sangyepa <sangyepa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 11:15:02 by sangyepa          #+#    #+#             */
-/*   Updated: 2023/04/04 11:18:54 by sangyepa         ###   ########.fr       */
+/*   Updated: 2023/04/04 21:42:32 by sangyepa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h"
 
+static int	ft_strncmp(char *s1, char *s2, size_t len)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && i < len)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	if (i == len)
+		i--;
+	return (s1[i] - s2[i]);
+}
+
 char	*strnstr(const char *haystack, const char *needle, size_t len)
 {
+	int	i;
+	int	j;
+	int	same;
+
 	
 }
