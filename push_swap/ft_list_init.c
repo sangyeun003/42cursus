8fcb642fb45f4f28e434e351a0521a81691579bd
SangYeun Park <sangyeun003@gmail.com>
2023-08-02 21:52:55 +0900

    push_swap 연결리스트 만듦

--- push_swap/ft_list_init.c
+++ push_swap/ft_list_init.c
@@ -6,7 +6,7 @@
 /*   By: sangyepa <sangyepa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 21:19:02 by sangyepa          #+#    #+#             */
-/*   Updated: 2023/08/02 21:24:01 by sangyepa         ###   ########.fr       */
+/*   Updated: 2023/08/02 21:52:13 by sangyepa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,14 @@
 
 void	ft_list_init(List *plist)
 {
-	plist->head = (Node *)malloc(sizeof(Node));	// 더미 노드
+	plist->head = (Node *)malloc(sizeof(Node));
 	plist->head->data = 0;
 	plist->head->prev = 0;
 	plist->head->next = plist->tail;
-
-	plist->tail = (Node *)malloc(sizeof(Node));	// 더미 노드
+	plist->tail = (Node *)malloc(sizeof(Node));
 	plist->tail->data = 0;
 	plist->tail->prev = plist->head;
 	plist->tail->next = 0;
-
 	plist->cur = 0;
 	plist->numOfData = 0;
 	return ;
--- push_swap/ft_list_remove.c
+++ push_swap/ft_list_remove.c
@@ -6,7 +6,7 @@
 /*   By: sangyepa <sangyepa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 21:37:44 by sangyepa          #+#    #+#             */
-/*   Updated: 2023/08/02 21:45:43 by sangyepa         ###   ########.fr       */
+/*   Updated: 2023/08/02 21:51:53 by sangyepa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,7 @@ Data	ft_list_remove(List *plist)
 
 	plist->cur->prev->next = plist->cur->next;
 	plist->cur->next->prev = plist->cur->prev;
-
-	plist->cur = plist->cur->prev;	// cur 위치 재조정
-
+	plist->cur = plist->cur->prev;
 	free(rpos);
 	plist->numOfData--;
 	return (rdata);
