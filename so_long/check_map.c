57646f7318161a9c164579930b6f37333fab2847
sangyeunpark <sangyeun003@gmail.com>
2023-09-21 14:16:35 +0900

    dd

--- so_long/check_map.c
+++ so_long/check_map.c
@@ -6,16 +6,16 @@
 /*   By: sangyepa <sangyepa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 16:22:25 by sangyepa          #+#    #+#             */
-/*   Updated: 2023/09/16 16:26:33 by sangyepa         ###   ########.fr       */
+/*   Updated: 2023/09/20 11:57:14 by sangyepa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"so_long.h"
 
-int	check_map(t_game game)
-{
-	if (game.width * game.height != (ft_strlen(game.map_str)))
-		return (FALSE);
-	// 첫 width개, 마지막 width개 모두 벽(1)이어야 함.
-	// 각 행의 처음과 마지막 벽(0)이어야 함.
-}
+// int	check_map(t_game game)
+// {
+// 	if (game.width * game.height != (ft_strlen(game.map_str)))
+// 		return (FALSE);
+// 	// 첫 width개, 마지막 width개 모두 벽(1)이어야 함.
+// 	// 각 행의 처음과 마지막 벽(0)이어야 함.
+// }
