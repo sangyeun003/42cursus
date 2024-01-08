d1a1354a1ba9ee0a36ab74ca0ff99dbfb219ba2a
sangyeunpark <sangyeun003@gmail.com>
2024-01-08 21:38:20 +0900

    gnl ing

--- exam03/practice/gnl.c
+++ exam03/practice/gnl.c
@@ -5,4 +5,14 @@
 # define	BUFFER_SIZE	1
 #endif
 
-char	*get_next_line(int fd);
+char	*get_next_line(int fd)
+{
+	static char	buf[BUFFER_SIZE + 1];
+	char		*line;
+	char		*newline;
+	int			count_read;
+	int			to_copy;
+
+	line = ft_strdup(buf);
+	while ()
+}
